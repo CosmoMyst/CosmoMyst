3785e06ef070845363a181d7113588499ec27b38
CodeMyst <code@myst.rs>
2022-03-07 23:10:59 +0100

    drawing rectangles using a texture instead

--- source/cosmomyst/graphics/impl/sdl/sdl_renderer.d
+++ source/cosmomyst/graphics/impl/sdl/sdl_renderer.d
@@ -9,12 +9,25 @@ import cosmomyst.graphics.renderer;
 import cosmomyst.graphics.color;
 import cosmomyst.graphics.impl.sdl;
 
+private struct DrawCall
+{
+    public uint sortOrder;
+}
+
 public class SDLRenderer : Renderer
 {
     private SDL_Renderer* renderer;
     private Color clearColor = Colors.black;
 
-    public this(SDLWindow window)
+    private DrawCall[] drawCalls;
+
+    private SDL_Texture* rectTex;
+
+    /++
+     + Params:
+     +   maxDrawCalls = Specifies the max amount of draw calls that can be executed per frame.
+     +/
+    public this(SDLWindow window, uint maxDrawCalls = 2000)
     {
         import core.stdc.stdlib : exit;
         import std.stdio : writeln;
@@ -28,10 +41,24 @@ public class SDLRenderer : Renderer
         }
 
         SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+
+        drawCalls.length = maxDrawCalls;
+
+        // create a texture used for drawing rectangles
+        rectTex = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 1, 1);
+
+        SDL_SetRenderTarget(renderer, rectTex);
+        setSDLDrawColor(Colors.white);
+        SDL_RenderClear(renderer);
+        SDL_SetRenderTarget(renderer, null);
+        setSDLDrawColor(clearColor);
+
+        SDL_SetTextureBlendMode(rectTex, SDL_BLENDMODE_BLEND);
     }
 
     public ~this()
     {
+        SDL_DestroyTexture(rectTex);
         SDL_DestroyRenderer(renderer);
     }
 
@@ -64,20 +91,24 @@ public class SDLRenderer : Renderer
 
         return Rectf(rect.x, rect.y, rect.w, rect.h);
     }
-    
-    public void drawFillRect(Rectf rect, Color color) @nogc nothrow
-    {
-        setSDLDrawColor(color);       
-
-        const sdlRect = toSDLRect(rect);
-        SDL_RenderFillRect(renderer, &sdlRect);
-
-        setSDLDrawColor(clearColor);
-    }
 
     public SDL_Renderer* getInternalRenderer() @nogc nothrow
     {
         return renderer;
+    }
+
+    public void drawFillRect(Rectf rect, Color color) @nogc nothrow
+    {
+        SDL_Rect source = { 0, 0, 1, 1 };
+        SDL_Rect dest = { cast(int) rect.x, cast(int) rect.y, cast(int) rect.w, cast(int) rect.h };
+
+        SDL_SetTextureColorMod(rectTex, color.r, color.g, color.b);
+        SDL_SetTextureAlphaMod(rectTex, color.a);
+
+        SDL_RenderCopy(renderer, rectTex, &source, &dest);
+
+        SDL_SetTextureColorMod(rectTex, Colors.white.r, Colors.white.g, Colors.white.b);
+        SDL_SetTextureAlphaMod(rectTex, 255);
     }
 
     public void drawSprite(Sprite sprite, Rectf source, Rectf dest) @nogc nothrow
